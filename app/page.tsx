af3568d9e6918ab9442af96bcb6941982e1eb8db
mdromi <mdromi14@gmail.com>
2023-11-29 22:20:10 +0600

    setup  home page product card

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-import Image from 'next/image'
-import Container from './components/Container'
-import HomeBanner from './components/HomeBanner'
+import Image from "next/image";
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
+import { products } from "@/utils/products";
+import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
-    <div className='p-8'>
+    <div className="p-8">
       <Container>
         <div>
-          <HomeBanner/>
+          <HomeBanner />
+        </div>
+        <div className="grid gird-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product: any) => {
+            return <ProductCard key={product.id} data={product}/>
+          })}
         </div>
       </Container>
     </div>
-  )
+  );
 }
-
