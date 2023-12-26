42f50e0dfacd6c1e08421576854f0753ff86a467
mdromi <mdromi14@gmail.com>
2023-12-27 01:58:47 +0600

    [Feature] Implement Orders, Products, Users Status Functionality

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,20 @@
-const Admin = () => {
-    return ( <div className="p-8">Admin Page</div> );
-}
- 
+import getOrders from "@/actions/getOrders";
+import Summary from "./Summary";
+import getProducts from "@/actions/getProducts";
+import getUsers from "@/actions/getUsers";
+import Container from "../components/Container";
+
+const Admin = async () => {
+  const products = await getProducts({ category: null });
+  const orders = await getOrders();
+  const users = await getUsers();
+  return (
+    <div className="p-8">
+      <Container>
+        <Summary users={users} products={products} orders={orders} />
+      </Container>
+    </div>
+  );
+};
+
 export default Admin;
