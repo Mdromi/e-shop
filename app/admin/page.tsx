566e84873efa1661045d21e12e7157a3c25281c7
mdromi <mdromi14@gmail.com>
2023-12-27 04:01:42 +0600

    [Feature] Implement Sales Status Graph Chart Functionality

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,15 +3,22 @@ import Summary from "./Summary";
 import getProducts from "@/actions/getProducts";
 import getUsers from "@/actions/getUsers";
 import Container from "../components/Container";
+import BarGraph from "./BarGraph";
+import getGraphData from "@/actions/getGraphData";
 
 const Admin = async () => {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
+  const graphData = await getGraphData();
+
   return (
     <div className="p-8">
       <Container>
         <Summary users={users} products={products} orders={orders} />
+        <div className="mt-4 mx-auto mx-w-[1100px]">
+          <BarGraph data={graphData} />
+        </div>
       </Container>
     </div>
   );
